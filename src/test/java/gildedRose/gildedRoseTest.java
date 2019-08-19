28167622949c2420b64219841b2368764de45fd4
DALLIN GUO (ITA-ISDC-ISD-OOCLL/ZHA) <GUODA@OOCL.COM>
2019-08-19 18:38:53 +0800

    [test_two]:sellin_is_0_and_quality_is_15_name_is_dallin

--- src/test/java/gildedRose/gildedRoseTest.java
+++ src/test/java/gildedRose/gildedRoseTest.java
@@ -15,6 +15,27 @@ public class gildedRoseTest {
     assertEquals(14,items[0].quality);
   }
 
+  @Test
+  public void should_return_10_and_25_when_sellin_is_0_and_quality_is_15_name_is_dallin(){
+    Item item = new Item("Dallin",0,15);
+    Item[] items = new Item[]{item};
+    GildedRose gildedRose = new GildedRose(items);
+    gildedRose.updateQuality();
+    assertEquals(-1,items[0].sellIn);
+    assertEquals(13,items[0].quality);
+  }
+
+
+//  @Test
+//  public void should_return_10_and_25_when_sellin_is_10_and_quality_is_15_name_is_Backstage_passes_to_a_TAFKAL80ETC_concert(){
+//    Item item = new Item("Dallin",10,15);
+//    Item[] items = new Item[]{item};
+//    GildedRose gildedRose = new GildedRose(items);
+//    gildedRose.updateQuality();
+//    assertEquals(9,items[0].sellIn);
+//    assertEquals(14,items[0].quality);
+//  }
+
 
 
 }
