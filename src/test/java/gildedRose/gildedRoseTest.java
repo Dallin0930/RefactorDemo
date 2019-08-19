805d90c1964fc1c0211f9d09e9845fc9836e896c
DALLIN GUO (ITA-ISDC-ISD-OOCLL/ZHA) <GUODA@OOCL.COM>
2019-08-19 18:48:32 +0800

    [test_four]:sellin_is_3_and_quality_is_30_name_is_Backstage_passes_to_a_TAFKAL80ETC_concert

--- src/test/java/gildedRose/gildedRoseTest.java
+++ src/test/java/gildedRose/gildedRoseTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 public class gildedRoseTest {
   @Test
-  public void should_return_10_and_25_when_sellin_is_10_and_quality_is_15_name_is_dallin(){
+  public void should_return_9_and_14_when_sellin_is_10_and_quality_is_15_name_is_dallin(){
     Item item = new Item("Dallin",10,15);
     Item[] items = new Item[]{item};
     GildedRose gildedRose = new GildedRose(items);
@@ -16,7 +16,7 @@ public class gildedRoseTest {
   }
 
   @Test
-  public void should_return_10_and_25_when_sellin_is_0_and_quality_is_15_name_is_dallin(){
+  public void should_return_fu1_and_13_when_sellin_is_0_and_quality_is_15_name_is_dallin(){
     Item item = new Item("Dallin",0,15);
     Item[] items = new Item[]{item};
     GildedRose gildedRose = new GildedRose(items);
@@ -26,7 +26,7 @@ public class gildedRoseTest {
   }
 
   @Test
-  public void should_return_10_and_25_when_sellin_is_no_more_than0_and_quality_is_15_name_is_dallin(){
+  public void should_return__and_13_when_sellin_is_no_more_than0_and_quality_is_15_name_is_dallin(){
     Item item = new Item("Dallin",-1,15);
     Item[] items = new Item[]{item};
     GildedRose gildedRose = new GildedRose(items);
@@ -36,13 +36,13 @@ public class gildedRoseTest {
   }
 
   @Test
-  public void should_return_10_and_25_when_sellin_is_10_and_quality_is_15_name_is_Backstage_passes_to_a_TAFKAL80ETC_concert(){
-    Item item = new Item("Dallin",10,15);
+  public void should_return_4_and_31_when_sellin_is_3_and_quality_is_30_name_is_Backstage_passes_to_a_TAFKAL80ETC_concert(){
+    Item item = new Item("Backstage passes to a TAFKAL80ETC concert",3,30);
     Item[] items = new Item[]{item};
     GildedRose gildedRose = new GildedRose(items);
     gildedRose.updateQuality();
-    assertEquals(9,items[0].sellIn);
-    assertEquals(14,items[0].quality);
+    assertEquals(2,items[0].sellIn);
+    assertEquals(31,items[0].quality);
   }
 
 
